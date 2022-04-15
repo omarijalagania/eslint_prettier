a464decaefb7cb877ee4a823a0276a98774e7462
omari <omari.jalagania@gmail.com>
2022-04-15 13:04:44 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import './App.css'
-import Home from './components/Home/Home'
 
 function App() {
-  return (
-    <div className='App'>
-      <Home />
-    </div>
-  )
+  return <div className='App'>Test</div>
 }
 
 export default App
