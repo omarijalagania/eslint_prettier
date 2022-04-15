187a3f54f524f729fad4860af9790eab6890ae14
omari <omari.jalagania@gmail.com>
2022-04-15 15:45:48 +0400

    code refactor

--- src/App.js
+++ src/App.js
@@ -1,53 +1,11 @@
-import React, { useContext } from 'react';
-import { FormContext } from './store/RegisterContext/formContext';
-import { useForm } from 'react-hook-form';
-import Input from './components/Ui/Input';
-import Error from './components/Ui/Error';
-import Button from './components/Ui/Button';
+import React from 'react';
+import Home from './pages/Home/Home';
 
 function App() {
-  // formState: { errors }
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-
-  const { onSubmit } = useContext(FormContext);
-
   return (
-    <div className='flex flex-col max-w-md mx-auto mt-20'>
-      <form
-        className='p-10 shadow-md flex flex-col space-y-4'
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <Input
-          className={`sm:text-sm border-2 p-2 outline-none ${
-            errors.name ? 'border-red-700' : 'border-gray-300'
-          } rounded-md`}
-          type='text'
-          name='name'
-          placeholder='Name'
-          register={register}
-        />
-        {errors.name && <Error>{errors.email.message}</Error>}
-
-        {/* <input */}
-
-        <Input
-          className={`sm:text-sm border-2 p-2 outline-none ${
-            errors.email ? 'border-red-700' : 'border-gray-300'
-          }  rounded-md`}
-          type='email'
-          name='email'
-          placeholder='Email'
-          register={register}
-        />
-        {errors.email && <Error>{errors.email.message}</Error>}
-        <Button color='blue' type='submit' text='Submit' />
-      </form>
-    </div>
+    <>
+      <Home />
+    </>
   );
 }
 
---
+++ src/components/Register/Register.js
@@ -0,0 +1,52 @@
+import React, { useContext } from 'react';
+import { FormContext } from '../../store/RegisterContext/formContext';
+import { useForm } from 'react-hook-form';
+import Input from '../Ui/Input';
+import Error from '../Ui/Error';
+import Button from '../Ui/Button';
+
+const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const { onSubmit } = useContext(FormContext);
+
+  return (
+    <div className='flex flex-col max-w-md mx-auto mt-20'>
+      <form
+        className='p-10 shadow-md flex flex-col space-y-4'
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <Input
+          className={`sm:text-sm border-2 p-2 outline-none ${
+            errors.name ? 'border-red-700' : 'border-gray-300'
+          } rounded-md`}
+          type='text'
+          name='name'
+          placeholder='Name'
+          register={register}
+        />
+        {errors.name && <Error>{errors.email.message}</Error>}
+
+        {/* <input */}
+
+        <Input
+          className={`sm:text-sm border-2 p-2 outline-none ${
+            errors.email ? 'border-red-700' : 'border-gray-300'
+          }  rounded-md`}
+          type='email'
+          name='email'
+          placeholder='Email'
+          register={register}
+        />
+        {errors.email && <Error>{errors.email.message}</Error>}
+        <Button color='blue' type='submit' text='Subscribe' />
+      </form>
+    </div>
+  );
+};
+
+export default Register;
